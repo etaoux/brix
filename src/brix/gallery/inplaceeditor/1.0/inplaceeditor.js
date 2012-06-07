cfbfadb23d0a7f4b263432226e3e87eca43cba12
keyapril <keyapril@gmail.com>
2012-06-07 11:50:01 +0800

    add inplaceeditor
    add inplaceeditor,chunk add rendered event

---
+++ src/brix/gallery/inplaceeditor/1.0/inplaceeditor.js
@@ -0,0 +1,53 @@
+KISSY.add("brix/gallery/inplaceeditor/1.0/inplaceeditor", function(S, Brick) {
+    function InplaceEditor() {
+        InplaceEditor.superclass.constructor.apply(this, arguments);
+    }
+    InplaceEditor.ATTACH = {
+        "input": {
+            focusout: function(e) {
+                this.hide();
+            },
+            keydown: function(e) {
+                if (e.keyCode == 13) {
+                    this.hide();
+                }
+            }
+        }
+    };
+
+    S.extend(InplaceEditor, Brick, {
+        v: null,//记录编辑原始值
+        show: function(x, y, v) {
+            var el = this.get('el');
+            el.css({
+                visibility: 'visible',
+                left: x,
+                top: y
+            });
+            this.v = v;
+            el.one('input')[0].focus();
+            el.one('input').val(v);
+        },
+        hide: function() {
+            var el = this.get('el');
+            el.css({
+                visibility: 'hidden',
+                left: '-9999px',
+                top: '-9999px'
+            });
+            var v = this.getValue();
+            if (this.v != v) {//值不相等时候出发valueChange事件
+                this.fire('valueChange', {
+                    value: v
+                });
+            }
+        },
+        getValue: function() {
+            var el = this.get('el');
+            return el.one('input').val();
+        }
+    });
+    return InplaceEditor;
+}, {
+    requires: ["brix/brick", "./inplaceeditor.css"]
+});
