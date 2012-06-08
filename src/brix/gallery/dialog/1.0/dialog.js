dc6e58cb190b7967826a4f242740123d5e5c65de
keyapril <keyapril@gmail.com>
2012-06-08 18:08:15 +0800

    code format

--- src/brix/gallery/dialog/1.0/dialog.js
+++ src/brix/gallery/dialog/1.0/dialog.js
@@ -1,148 +1,122 @@
-KISSY.add("brix/gallery/dialog/1.0/dialog", function(S,Pagelet,Overlay) {
-    function _getContent(v,dir){
+KISSY.add("brix/gallery/dialog/1.0/dialog", function(S, Pagelet, Overlay) {
+    function _getContent(v, dir) {
         var wrapper = {
-                    'up': [
-                        '<div class="popup popup-up">',
-                            '<div class="popup-left"></div>',
-                            '<div class="popup-center">',
-                                '<div class="popup-content">'+v+'</div>',
-                            '</div>',
-                            '<div class="popup-right"></div>',
-                            '<div class="popup-bottom">',
-                                '<div class="popup-bottom-left"></div>',
-                                '<div class="popup-bottom-center">&nbsp;</div>',
-                                '<div class="popup-bottom-right"></div>',
-                            '</div>',
-                        '</div>'
-                    ],
-                    'right': [
-                        '<div class="popup-h popup-h-right">',
-                            '<div class="popup-h-top"></div>',
-                            '<div class="popup-h-center">',
-                                '<div class="popup-h-body">',
-                                    '<div class="popup-content">'+v+'</div>',
-                                '</div>',
-                            '</div>',
-                            '<div class="popup-h-bottom"></div>',
-                        '</div>'
-                    ]
-                };
-                wrapper['down'] = wrapper['up'].slice(0);
-                wrapper['down'][0] = wrapper['down'][0].replace('popup-up', '');
-                wrapper['left'] = wrapper['right'].slice(0);
-                wrapper['left'][0] = wrapper['left'][0].replace('popup-h-right', '');
-
-                return wrapper[ dir ].join('');
+            'up': ['<div class="popup popup-up">', '<div class="popup-left"></div>', '<div class="popup-center">', '<div class="popup-content">' + v + '</div>', '</div>', '<div class="popup-right"></div>', '<div class="popup-bottom">', '<div class="popup-bottom-left"></div>', '<div class="popup-bottom-center">&nbsp;</div>', '<div class="popup-bottom-right"></div>', '</div>', '</div>'],
+            'right': ['<div class="popup-h popup-h-right">', '<div class="popup-h-top"></div>', '<div class="popup-h-center">', '<div class="popup-h-body">', '<div class="popup-content">' + v + '</div>', '</div>', '</div>', '<div class="popup-h-bottom"></div>', '</div>']
+        };
+        wrapper['down'] = wrapper['up'].slice(0);
+        wrapper['down'][0] = wrapper['down'][0].replace('popup-up', '');
+        wrapper['left'] = wrapper['right'].slice(0);
+        wrapper['left'][0] = wrapper['left'][0].replace('popup-h-right', '');
+        return wrapper[dir].join('');
     }
-    function _setWidth(v,dir){
-        switch(dir){
-            case 'up':
-            case 'down':
-                if(v<320){
-                    return 320;
-                }
-                break;
-            case 'left':
-            case 'right':
-                if(v<145){
-                    return 145;
-                }
-                break;
+
+    function _setWidth(v, dir) {
+        switch (dir) {
+        case 'up':
+        case 'down':
+            if (v < 320) {
+                return 320;
+            }
+            break;
+        case 'left':
+        case 'right':
+            if (v < 145) {
+                return 145;
+            }
+            break;
         }
         return v;
     }
+
     function Dialog(config) {
         var self = this;
         Dialog.superclass.constructor.apply(this, arguments);
     }
     Dialog.ATTRS = {
-        start:{
-            value:{
-                left:600,
-                top:100,
-                opacity:0
+        start: {
+            value: {
+                left: 600,
+                top: 100,
+                opacity: 0
             }
         },
-        end:{
-            value:{
-                left:100,
-                top:100,
-                opacity:1
+        end: {
+            value: {
+                left: 100,
+                top: 100,
+                opacity: 1
             }
         },
-        x:{
-            getter:function(){
+        x: {
+            getter: function() {
                 var self = this;
                 return self.get('start').left;
             }
         },
-        y:{
-            getter:function(){
+        y: {
+            getter: function() {
                 var self = this;
                 return self.get('start').top;
             }
         },
-        dir:{
-            getter:function(){
+        dir: {
+            getter: function() {
                 var self = this;
-                var dir ='',
-                start = self.get('start'),
-                end = self.get('end');
-                dir = start.left === end.left &&
-                            ( start.top < end.top ? 'down' : 'up' )
-                      || start.top === end.top &&
-                            ( start.left < end.left ? 'right' : 'left' )
-                        || 'left';
+                var dir = '',
+                    start = self.get('start'),
+                    end = self.get('end');
+                dir = start.left === end.left && (start.top < end.top ? 'down' : 'up') || start.top === end.top && (start.left < end.left ? 'right' : 'left') || 'left';
                 return dir;
             }
         },
-        width:{
-            valueFn:function(v){
-                if(!v){
+        width: {
+            valueFn: function(v) {
+                if (!v) {
                     return v;
                 }
                 var self = this;
                 var dir = self.get('dir');
-                return _setWidth(v,dir);
+                return _setWidth(v, dir);
             },
-            setter:function(v){
+            setter: function(v) {
                 var self = this;
                 var dir = self.get('dir');
-                return _setWidth(v,dir);
+                return _setWidth(v, dir);
             }
         },
-        elCls:{
-            valueFn:function(){
-                return  'dialog-' + this.get('dir');
+        elCls: {
+            valueFn: function() {
+                return 'dialog-' + this.get('dir');
             }
         },
-        prefixCls : {
-            value:'ux-'
+        prefixCls: {
+            value: 'ux-'
         },
-        duration :{
+        duration: {
             value: 0.3
         },
-        easing : {
-            value:'easeOut'
+        easing: {
+            value: 'easeOut'
         },
         closable: {
-            value:true
+            value: true
         },
         mask: {
-            value:false
+            value: false
         },
-        content:{
-            valueFn:function(v){
-                return _getContent('',this.get('dir'));
+        content: {
+            valueFn: function(v) {
+                return _getContent('', this.get('dir'));
             },
-            setter:function(v){
-                return _getContent(v,this.get('dir'));
+            setter: function(v) {
+                return _getContent(v, this.get('dir'));
             }
         },
-        tmpl:{
-            value:null
+        tmpl: {
+            value: null
         },
-        data:{
+        data: {
 
         }
     };
@@ -172,13 +146,13 @@ KISSY.add("brix/gallery/dialog/1.0/dialog", function(S,Pagelet,Overlay) {
                 }
             });
             //渲染模板内容
-            self.on('afterRenderUI',function(){
-                if(self.get('tmpl')){
+            self.on('afterRenderUI', function() {
+                if (self.get('tmpl')) {
                     self.pagelet = new Pagelet({
-                        render:self.get('contentEl').one('.popup-content'),
-                        autoRender:true,
-                        tmpl:self.get('tmpl'),
-                        data:self.get('data')
+                        render: self.get('contentEl').one('.popup-content'),
+                        autoRender: true,
+                        tmpl: self.get('tmpl'),
+                        data: self.get('data')
                     });
                     self.pagelet.addBehavior();
                 }
@@ -187,5 +161,5 @@ KISSY.add("brix/gallery/dialog/1.0/dialog", function(S,Pagelet,Overlay) {
     });
     return Dialog;
 }, {
-    requires: ["brix/pagelet","overlay","./dialog.css"]
+    requires: ["brix/pagelet", "overlay", "./dialog.css"]
 });
--- src/brix/gallery/kwicks/1.0/kwicks.js
+++ src/brix/gallery/kwicks/1.0/kwicks.js
@@ -1,8 +1,8 @@
 KISSY.add("brix/gallery/kwicks/1.0/kwicks", function(S, Brick) {
     function Kwicks(config) {
         var self = this;
-        self.on('rendered',function(){
-           self.initialize();
+        self.on('rendered', function() {
+            self.initialize();
         });
         Kwicks.superclass.constructor.apply(this, arguments);
     }
@@ -24,8 +24,8 @@ KISSY.add("brix/gallery/kwicks/1.0/kwicks", function(S, Brick) {
             value: 'mouseenter'
         },
         //激活样式
-        activeCls:{
-            value:'active'
+        activeCls: {
+            value: 'active'
         },
         //间距
         spacing: {
@@ -36,95 +36,94 @@ KISSY.add("brix/gallery/kwicks/1.0/kwicks", function(S, Brick) {
             value: 0.3
         },
         //动画效果
-        easing:{
-            value:'easeNone'
+        easing: {
+            value: 'easeNone'
         },
         //最大值
-        max:{
+        max: {
 
         },
         //最小值
-        min:{
+        min: {
 
         }
     };
 
     S.extend(Kwicks, Brick, {
-        initialize:function(){
+        initialize: function() {
             var self = this,
-            isVertical = self.get('isVertical'),
-            sticky = self.get('sticky'),
-            defaultKwick = self.get('defaultKwick'),
-            triggerType = self.get('triggerType'),
-            activeCls = self.get('activeCls'),
-            spacing = self.get('spacing'),
-            duration = self.get('duration'),
-            easing = self.get('easing'),
-            max = self.get('max'),
-            min = self.get('min');
+                isVertical = self.get('isVertical'),
+                sticky = self.get('sticky'),
+                defaultKwick = self.get('defaultKwick'),
+                triggerType = self.get('triggerType'),
+                activeCls = self.get('activeCls'),
+                spacing = self.get('spacing'),
+                duration = self.get('duration'),
+                easing = self.get('easing'),
+                max = self.get('max'),
+                min = self.get('min');
 
             var WoH = (isVertical ? 'height' : 'width'); // WoH = Width or Height
             var LoT = (isVertical ? 'top' : 'left'); // LoT = Left or Top
             var container = self.get('el');
             var kwicks = container.all('li');
             var length = kwicks.length;
-            var normWoH = kwicks.item(0).css(WoH).replace(/px/,''); // normWoH = Normal Width or Height
-
-            if(!max) {
+            var normWoH = kwicks.item(0).css(WoH).replace(/px/, ''); // normWoH = Normal Width or Height
+            if (!max) {
                 max = (normWoH * length) - (min * (length - 1));
             } else {
                 min = ((normWoH * length) - max) / (length - 1);
             }
             // set width of container ul
-            if(isVertical) {
+            if (isVertical) {
                 container.css({
-                    width : kwicks.item(0).css('width'),
-                    height : (normWoH * length) + (spacing * (length - 1)) + 'px'
+                    width: kwicks.item(0).css('width'),
+                    height: (normWoH * length) + (spacing * (length - 1)) + 'px'
                 });
             } else {
                 container.css({
-                    width : (normWoH * length) + (spacing * (length - 1)) + 'px',
-                    height : kwicks.item(0).css('height')
+                    width: (normWoH * length) + (spacing * (length - 1)) + 'px',
+                    height: kwicks.item(0).css('height')
                 });
             }
 
             // pre calculate left or top values for all kwicks but the first and last
             // i = index of currently hovered kwick, j = index of kwick we're calculating
             var preCalcLoTs = []; // preCalcLoTs = pre-calculated Left or Top's
-            for(i = 0; i < length; i++) {
+            for (i = 0; i < length; i++) {
                 preCalcLoTs[i] = [];
                 // don't need to calculate values for first or last kwick
-                for(j = 1; j < length - 1; j++) {
-                    if(i == j) {
+                for (j = 1; j < length - 1; j++) {
+                    if (i == j) {
                         preCalcLoTs[i][j] = isVertical ? j * min + (j * spacing) : j * min + (j * spacing);
                     } else {
-                        preCalcLoTs[i][j] = (j <= i ? (j * min) : (j-1) * min + max) + (j * spacing);
+                        preCalcLoTs[i][j] = (j <= i ? (j * min) : (j - 1) * min + max) + (j * spacing);
                     }
                 }
             }
 
             // loop through all kwick elements
-            kwicks.each(function(kwick,i) {
+            kwicks.each(function(kwick, i) {
                 // set initial width or height and left or top values
                 // set first kwick
-                if(i === 0) {
+                if (i === 0) {
                     kwick.css(LoT, '0px');
                 }
                 // set last kwick
-                else if(i == length - 1) {
+                else if (i == length - 1) {
                     kwick.css(isVertical ? 'bottom' : 'right', '0px');
                 }
                 // set all other kwicks
                 else {
-                    if(sticky) {
+                    if (sticky) {
                         kwick.css(LoT, preCalcLoTs[defaultKwick][i]);
                     } else {
                         kwick.css(LoT, (i * normWoH) + (i * spacing));
                     }
                 }
                 // correct size in sticky mode
-                if(sticky) {
-                    if(defaultKwick == i) {
+                if (sticky) {
+                    if (defaultKwick == i) {
                         kwick.css(WoH, max + 'px');
                         kwick.addClass(activeCls);
                     } else {
@@ -139,35 +138,34 @@ KISSY.add("brix/gallery/kwicks/1.0/kwicks", function(S, Brick) {
                 kwick.on(triggerType, function() {
                     kwicks.stop().removeClass(activeCls);
                     kwick.addClass(activeCls);
-                    kwicks.each(function(k,j) {
+                    kwicks.each(function(k, j) {
                         var animObj = {};
-                        if(kwick[0]==k[0]){
+                        if (kwick[0] == k[0]) {
                             animObj[WoH] = max;
-                            if(j > 0 && j < length - 1) {
+                            if (j > 0 && j < length - 1) {
                                 animObj[LoT] = preCalcLoTs[i][j];
                             }
-                        }
-                        else{
-                            animObj[WoH]=min;
-                            if(j > 0 && j < length - 1) {
+                        } else {
+                            animObj[WoH] = min;
+                            if (j > 0 && j < length - 1) {
                                 animObj[LoT] = preCalcLoTs[i][j];
                             }
 
                         }
-                        k.animate(animObj,duration,easing);
+                        k.animate(animObj, duration, easing);
                     });
                 });
             });
-            if(!sticky) {
+            if (!sticky) {
                 container.on("mouseleave", function() {
                     kwicks.stop().removeClass(activeCls);
-                    kwicks.each(function(k,j) {
+                    kwicks.each(function(k, j) {
                         var animObj = {};
-                            animObj[WoH]=normWoH;
-                            if(j < length - 1) {
-                                animObj[LoT] = (j * normWoH) + (j * spacing);
-                            }
-                        k.animate(animObj,duration,easing);
+                        animObj[WoH] = normWoH;
+                        if (j < length - 1) {
+                            animObj[LoT] = (j * normWoH) + (j * spacing);
+                        }
+                        k.animate(animObj, duration, easing);
                     });
                 });
             }
