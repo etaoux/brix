fd7a62facfdef7241b95d59dd0dfd5ea0f68a741
keyapril <keyapril@gmail.com>
2012-07-11 13:05:24 +0800

    fixed inDom bug

--- src/core/tmpler.js
+++ src/core/tmpler.js
@@ -22,7 +22,7 @@ KISSY.add("brix/core/tmpler", function(S, Mustache, Node) {
      */
 
     function _inDom(el) {
-        return S.one(document.body).contains(el);
+        return S.one(document).contains(el);
     }
 
     /**
