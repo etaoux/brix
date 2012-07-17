09e9946444d8aeeeb59531bbf3af93dd67f7d483
Jake Chen <jakeplus@gmail.com>
2012-07-17 15:52:06 +0800

    Site updated at 2012-07-17 07:52:06 UTC

---
+++ demo/tutorials/attach/helloworld.js
@@ -0,0 +1,41 @@
+KISSY.add('helloworld', function(S, Brick) {
+	function HelloWorld() {
+		HelloWorld.superclass.constructor.apply(this, arguments);
+	}
+	HelloWorld.ATTRS = {
+		name:{
+			value:'World'
+		}
+	};
+
+	HelloWorld.ATTACH = {
+		"":{
+			"click":function(){
+				alert('节点被点击');
+			}
+		},
+		"#spanName":{
+			"click":function(e){
+				e.halt();//阻止冒泡
+				this.set('name','I\'m Click')
+				S.one(e.currentTarget).html(this.get('name'));
+			}
+		}
+	};
+
+	HelloWorld.DOCATTACH = {
+		"":{
+			"click":function(){
+				alert('document被点击');
+			}
+		}
+	};
+
+	S.extend(HelloWorld, Brick, {
+
+	});
+
+	return HelloWorld;
+}, {
+	requires: ["brix/core/brick"]
+});
