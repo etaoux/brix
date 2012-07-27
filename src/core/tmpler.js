f0e592e82d0bd349c13ec617ed5a8c12b56aeb82
keyapril <keyapril@gmail.com>
2012-07-27 11:09:57 +0800

    同步最新版本

--- src/core/tmpler.js
+++ src/core/tmpler.js
@@ -36,17 +36,17 @@ KISSY.add("brix/core/tmpler", function(S, Mustache, Node,UA) {
         //去掉attr="",谁可以优化一下这个正则？
         html = html.replace(/((\{{2,3}\#(.+)?\}{2,3})([\s\S]*)?\s*(\{{2,3}~\3\}{2,3}))\=\"\"/g, '$1');
 
-        //对if语句的处理
+        //对if语句的还原处理
         html = html.replace(/(\{{2,3}[\^#~]?)iftmplbrick\_(\d+)(\}{2,3})/g, function(w, i, j, k) {
             return i + arr[parseInt(j,10)] + k;
         });
-
+        //对href和src语句的还原处理
+        html = html.replace(/(href|src)=("|')("|')/ig,"");
+        html = html.replace(/(\{{2,3}[\^#~]?)hreftmplbrick\_(\d+)(\}{2,3})/g, function(w, i, j, k) {
+            return arr[parseInt(j,10)];
+        });
         //将~符号替换回/，完美了。
         html = html.replace(/(\{{2,3})~/g, '$1/');
-        //修复火狐下对a标签href编码的bug
-        if(UA.firefox){
-            return decodeURIComponent(html);
-        }
         return html;
     }
 
@@ -101,6 +101,21 @@ KISSY.add("brix/core/tmpler", function(S, Mustache, Node,UA) {
                     }
                     return i + name + k;
                 });
+
+
+                //对href和src的处理
+                tmpl = tmpl.replace(/((href|src)=("|')(.*)?("|'))/ig,function(w,i){
+                    var index = S.indexOf(i, arr),
+                        name = 'hreftmplbrick_';
+                    if (index < 0) {
+                        name += arr.length;
+                        arr.push(i);
+                    } else {
+                        name += index;
+                    }
+                    return "{{#"+name+"}}"  ;
+                });
+
                 node = $(tmpl);
                 if (node.length > 1) { //如果是多个节点，则创建容器节点
                     node = $('<div></div>').append(node);
