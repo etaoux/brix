3ac8d4e78a95fb4714d3f5778da6eaf85a33181c
huanglong <huanglong.dbz@taobao.com>
2012-08-09 11:50:15 +0800

    add switchTo for switcher

--- src/gallery/switcher/index.js
+++ src/gallery/switcher/index.js
@@ -19,7 +19,8 @@ KISSY.add("brix/gallery/switcher/index", function(S, Brick) {
         '': {
             click: function(e) {
                 var el = this.get('el');
-                el._switchTo(el.addClass('switcher-on'));
+
+                this._switchTo(el.hasClass('switcher-on'));
             }
         }
     };
@@ -31,35 +32,39 @@ KISSY.add("brix/gallery/switcher/index", function(S, Brick) {
             self.on('afterRenderUI', function() {
                 var el = self.get('el');
 
-                if (self.get("switchOn")) {
-                    el._switchTo(true);
+                if (self.get("switcheOn")) {
+                    self._switchTo(true);
                 }
                 else {
-                    el._switchTo(false);
+                    self._switchTo(false);
                 }
                 if (self.get('tmpl')) {
                     self.pagelet = new Pagelet({
                         container: self.get('contentEl'),
                         autoRender: true,
-                        tmpl: self.get('tmpl'),
-                        data: self.get('data')
+                        tmpl: self.get('tmpl')
                     });
                     self.pagelet.addBehavior();
                 }
             });
         },
         _switchTo: function(on) {
-            var self = this;
+            var self = this,
+                el = self.get('el');
 
-            if (on) {
+            if (!on) {
                 el.addClass('switcher-on');
+                if (self.get('switchNoStatus')) {
+                    el.addClass('switcher-no-status');
+                }
             }
             else {
                 el.removeClass('switcher-on');
+                if (self.get('switchNoStatus')) {
+                    el.removeClass('switcher-no-status');
+                }
             }
-            if (self.get('switchNoStatus')) {
-                el.addClass('switcher-no-status');
-            }
+
             data = {
                 value: el.hasClass('switcher-on')
             };
