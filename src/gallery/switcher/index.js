8aa6f6b8481daf4316a490b1f9950b03217c927a
huanglong <huanglong.dbz@taobao.com>
2012-08-09 14:28:44 +0800

    add example and descrption for switcher

--- src/gallery/switcher/index.js
+++ src/gallery/switcher/index.js
@@ -4,7 +4,7 @@ KISSY.add("brix/gallery/switcher/index", function(S, Brick) {
     }
     Switcher.ATTRS = {
         switchOn: {
-            value: true
+            value: false
         },
         switchNoStatus: {
             value: false
@@ -20,39 +20,27 @@ KISSY.add("brix/gallery/switcher/index", function(S, Brick) {
             click: function(e) {
                 var el = this.get('el');
 
-                this._switchTo(el.hasClass('switcher-on'));
+                this._switchTo(!el.hasClass('switcher-on'));
             }
         }
     };
 
     S.extend(Switcher, Brick, {
-        initializer: function() {
+        initialize: function() {
             var self = this;
-            //渲染模板内容
-            self.on('afterRenderUI', function() {
-                var el = self.get('el');
 
-                if (self.get("switcheOn")) {
-                    self._switchTo(true);
-                }
-                else {
-                    self._switchTo(false);
-                }
-                if (self.get('tmpl')) {
-                    self.pagelet = new Pagelet({
-                        container: self.get('contentEl'),
-                        autoRender: true,
-                        tmpl: self.get('tmpl')
-                    });
-                    self.pagelet.addBehavior();
-                }
-            });
+            if (self.get("switchOn")) {
+                self._switchTo(true);
+            }
+            else {
+                self._switchTo(false);
+            }
         },
         _switchTo: function(on) {
             var self = this,
                 el = self.get('el');
 
-            if (!on) {
+            if (on) {
                 el.addClass('switcher-on');
                 if (self.get('switchNoStatus')) {
                     el.addClass('switcher-no-status');
