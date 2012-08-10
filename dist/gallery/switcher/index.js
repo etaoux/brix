4fc9ee044971e13c272546e2e2bcb8f958ea426b
Jake Chen <jakeplus@gmail.com>
2012-08-10 19:00:08 +0800

    Site updated at 2012-08-10 11:00:08 UTC

---
+++ dist/gallery/switcher/index.js
@@ -0,0 +1,67 @@
+KISSY.add("brix/gallery/switcher/index", function(S, Brick) {
+    function Switcher() {
+        Switcher.superclass.constructor.apply(this, arguments);
+    }
+    Switcher.ATTRS = {
+        switchOn: {
+            value: false
+        },
+        switchNoStatus: {
+            value: false
+        }
+    };
+
+    Switcher.METHOD = {
+
+    };
+
+    Switcher.ATTACH = {
+        '': {
+            click: function(e) {
+                var el = this.get('el');
+
+                this._switchTo(!el.hasClass('switcher-on'));
+            }
+        }
+    };
+
+    S.extend(Switcher, Brick, {
+        initialize: function() {
+            var self = this;
+
+            if (self.get("switchOn")) {
+                self._switchTo(true);
+            }
+            else {
+                self._switchTo(false);
+            }
+        },
+        _switchTo: function(on) {
+            var self = this,
+                el = self.get('el');
+
+            if (on) {
+                el.addClass('switcher-on');
+                if (self.get('switchNoStatus')) {
+                    el.addClass('switcher-no-status');
+                }
+            }
+            else {
+                el.removeClass('switcher-on');
+                if (self.get('switchNoStatus')) {
+                    el.removeClass('switcher-no-status');
+                }
+            }
+
+            data = {
+                value: el.hasClass('switcher-on')
+            };
+            this.fire('switch', data);
+        }
+    });
+
+    S.augment(Switcher,Switcher.METHOD);
+    return Switcher;
+}, {
+    requires: ["brix/core/brick"]
+});
