fd719a29f19b71f1d0124af828799596a33311cd
miaojing <miaojing@taobao.com>
2012-08-22 11:57:08 +0800

    add aria(for and position) for form

--- src/gallery/form/index.js
+++ src/gallery/form/index.js
@@ -7,7 +7,20 @@ KISSY.add("brix/gallery/form/index", function(S, Brick) {
     function Form() {
         Form.superclass.constructor.apply(this, arguments);
     }
-    
+    Form.ATTACH = {
+        '.checkbox, .radio': {
+            'keyup': function (e) {
+                if (e.keyCode === KeyCodes.ENTER ||
+                    e.keyCode === KeyCodes.SPACE) {
+                    S.one(e.target).fire('click');
+
+                    // 空格键会触发页面滚动.
+                    e.preventDefault();
+                }
+            }
+        }
+    };
+
     function handleStat(node, checkSiblings) {
         var input = node.one('.input');
         var _input = node.one('input');
@@ -34,7 +47,6 @@ KISSY.add("brix/gallery/form/index", function(S, Brick) {
     S.extend(Form, Brick, {
         initialize: function () {
             var el = this.get('el');
-
             el.addClass('bx-controls');
             el.all('.checkbox, .radio').each(handleStat);
             // 思路：不直接处理click事件，监听input状态变化来相应span的状态.
@@ -43,10 +55,8 @@ KISSY.add("brix/gallery/form/index", function(S, Brick) {
             });
             // 1. 坑爹的IE6/7/8只能在input被渲染时click可以从label传递到input
             // 2. 坑爹的IE6/7只能在label有对应for属性时click才能传递到input，嵌套都不行
-            // 3. 更好地支持无障碍访问 input必须和label通过for关联add by miaojing
             //    ref: http://lucassmith.name/2008/04/label-checkbox-concerns.html
             // 这里统一隐藏掉input
-            // 不可以隐藏input, 隐藏了读屏软件无法正常读取radio checkbox 该有的状态提示 add by miaojing
             if (S.UA.ie < 9) {
                 el.all('.checkbox, .radio').on('click', function (e) {
                     // 避免循环触发.
