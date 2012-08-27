69c43514441ca6d7612cf339b2c6160ced9240db
Jake Chen <jakeplus@gmail.com>
2012-08-27 11:28:55 +0800

    Update style, for affix page-nav bar

--- assets/js/style.js
+++ assets/js/style.js
@@ -10,6 +10,31 @@ KISSY.config({
         [/(.+brix)\/(.+?)(?:-min)?.css(\?[^?]+)?$/, "$1/src/$2.css$3"]
     ]
 });
+
+KISSY.ready(function(S) {
+
+    var affix = '#page-nav',
+        threshold = getThreshold(affix);
+
+    S.Event.on(window, 'scroll', function(ev) {
+        var body = S.one(window);
+
+        if (body.scrollTop() > threshold) {
+            S.one(affix).addClass('fixed');
+        }
+        else {
+            S.one(affix).removeClass('fixed');
+        }
+    });
+
+    function getThreshold(selector) {
+        var node = S.one(selector),
+            offset = node.offset();
+
+        return offset.top - node.outerHeight();
+    }
+});
+
 KISSY.use('brix/core/pagelet', function(S, Pagelet) {
     var pagelet = new Pagelet({
         tmpl: '#page' // 模板(容器节点)
