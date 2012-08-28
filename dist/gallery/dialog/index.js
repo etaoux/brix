a6509ca28266eaa82e7fc969a0b78b63dd63af5b
Jake Chen <jakeplus@gmail.com>
2012-08-28 11:03:25 +0800

    Site updated at 2012-08-28 03:03:25 UTC

--- dist/gallery/dialog/index.js
+++ dist/gallery/dialog/index.js
@@ -54,6 +54,9 @@ KISSY.add("brix/gallery/dialog/index", function(S, Pagelet, Overlay) {
                 var dir = '',
                     start = self.get('start'),
                     end = self.get('end');
+                if(start.left===end.left&&start.top===end.top){
+                    return false;
+                }
                 dir = start.left === end.left && (start.top < end.top ? 'down' : 'up') || start.top === end.top && (start.left < end.left ? 'right' : 'left') || 'left';
                 return dir;
             }
@@ -68,7 +71,7 @@ KISSY.add("brix/gallery/dialog/index", function(S, Pagelet, Overlay) {
             value: 0.3
         },
         easing: {
-            value: 'easeOut'
+            value: 'easeNone'
         },
         closable: {
             value: true
@@ -112,13 +115,16 @@ KISSY.add("brix/gallery/dialog/index", function(S, Pagelet, Overlay) {
             var self = this;
             var el = self.get('el'),
                 body = S.one('body'),
-                s = 'start';
+                html = S.one('html'),
+                s = 'start',
+                dir = self.get('dir');
             //移除动画队列，设置显示，为动画增加效果
             el.stop();
             el.css('visibility', 'visible');
             if (v) {//如果显示
                 el.css(self.get('start'));
-                s = 'end'
+                s = 'end';
+                
             }
             else{
                 el.css(self.get('end'));
@@ -126,22 +132,28 @@ KISSY.add("brix/gallery/dialog/index", function(S, Pagelet, Overlay) {
             }
             //为防止出现滚动条
             body.css({width:body.width(),height:body.height(),overflow:'hidden'});
+            html.css({width:body.width(),height:body.height(),overflow:'hidden'});
             el.animate(self.get(s), self.get('duration'), self.get('easing'), function() {
                 el.css('visibility', v?'visible':"hidden");
                 if(!v){
                     el.css({left:'-99999px',top:'-99999px'});
                 }
                 body.css({width:'',height:'',overflow:''});
+                html.css({width:'',height:'',overflow:''});
                 fn&&fn.call(self);
             });
         },
         bindUI:function(){
-            var self = this;
+            var self = this,el = self.get('el');
             self.on('beforeVisibleChange', function(ev) {
-                var v = ev.newVal;
+                var v = ev.newVal,dir = self.get('dir');
+                el.removeClass('dialog-left').removeClass('dialog-right').removeClass('dialog-up').removeClass('dialog-down');
+                if(dir){
+                    el.addClass('dialog-'+dir)
+                }
                 if(!v){
                     self._visibilityChange(v,function(){
-                        self.get('el').css('visibility', 'hidden');
+                        el.css('visibility', 'hidden');
                         self.set('visible',false,{silent:true});
                         self.fire('hide');
                     });
