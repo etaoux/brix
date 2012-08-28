1afeaa07de48eeb6adec37777a2e3c5bc00810d0
Jake Chen <jakeplus@gmail.com>
2012-08-28 13:41:01 +0800

    Site updated at 2012-08-28 05:41:01 UTC

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,14 +1,22 @@
-KISSY.ready(function(S) {
-    S.all('.j-demo').on('load', function(e) {
-        var DOM = S.DOM;
+    KISSY.all('.j-demo').on('load', function(e) {
+        var DOM = KISSY.DOM;
         var iframe = e.currentTarget,
             win = iframe.contentWindow,
             doc = win.document;
         var height = DOM.height(doc);
-        iframe.height = height<300?300:height;
-        var width = DOM.width(doc);
-        iframe.width = width<800?800:width;
+        iframe.height = height<450?450:height;
+        iframe.width ='100%';
+
+        //监听变化，动态修改iframe变化
+        (function(frame){
+            setInterval(function(){
+                var height = DOM.outerHeight(frame.contentWindow.document.body);
+                iframe.height = height<450?450:height;
+            },300);
+        })(iframe);
+        
     });
+    KISSY.ready(function(S) {
 	var log = log4javascript.getLogger("main");
 	var appender = new log4javascript.InPageAppender('J_log',true);
 	var logdivNode = S.one('#J_log');
