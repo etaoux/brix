386a1dd76435ca769e93296c583787bacb3577da
Jake Chen <jakeplus@gmail.com>
2012-08-29 12:20:32 +0800

    Site updated at 2012-08-29 04:20:32 UTC

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,22 +1,28 @@
-    KISSY.all('.j-demo').on('load', function(e) {
-        var DOM = KISSY.DOM;
-        var iframe = e.currentTarget,
-            win = iframe.contentWindow,
-            doc = win.document;
-        var height = DOM.height(doc);
-        iframe.height = height<450?450:height;
-        iframe.width ='100%';
+KISSY.all('.j-demo').on('load', function(e) {
+    var DOM = KISSY.DOM;
+    var iframe = e.currentTarget,
+        win = iframe.contentWindow,
+        doc = win.document;
+    var height = DOM.height(doc);
 
-        //监听变化，动态修改iframe变化
-        (function(frame){
-            setInterval(function(){
-                var height = DOM.outerHeight(frame.contentWindow.document.body);
-                iframe.height = height<450?450:height;
-            },300);
-        })(iframe);
-        
-    });
-    KISSY.ready(function(S) {
+    iframe.height = height < 450 ? 450 : height;
+    iframe.width ='100%';
+
+    //监听变化，动态修改iframe变化
+    (function(frame){
+        setInterval(function(){
+            var height = DOM.outerHeight(frame.contentWindow.document.body);
+
+            iframe.height = height < 450 ? 450 : height;
+        }, 300);
+    })(iframe);
+
+});
+
+KISSY.ready(function(S) {
+    if (!window.log4javascript) {
+        return;
+    }
 	var log = log4javascript.getLogger("main");
 	var appender = new log4javascript.InPageAppender('J_log',true);
 	var logdivNode = S.one('#J_log');
@@ -28,9 +34,9 @@
             log.info('隐藏调试窗口');
         	logdivNode.animate({width:'88px',height:'18px'},0.3,'easeNone',function(){
                 appender.hide();
-                closeNode.html('显示调试窗口'); 
+                closeNode.html('显示调试窗口');
             });
-            
+
         }
         else{
             log.info('显示调试窗口');
@@ -40,7 +46,7 @@
             });
         }
     });
-    
+
 	console = window.console || {};
 	console.log=function(){log.info.apply(log,arguments)};
 });
