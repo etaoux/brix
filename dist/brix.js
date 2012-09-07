30039b18ef3c79f2f4f0ca1097b7cd38b79b4b51
cricy <feiyelanghai@gmail.com>
2012-09-07 11:11:46 +0800

    merge master

--- dist/brix.js
+++ dist/brix.js
@@ -2,7 +2,7 @@
 * https://github.com/etaoux/brix
 * Copyright (c) 2012 etaoux; Licensed MIT */
 
-KISSY.config({packages:[{name: "brix",tag: "20120906",path: "http://a.tbcdn.cn/p/",charset: "utf-8"}]});
+KISSY.config({packages:[{name: "brix",tag: "20120907",path: "http://a.tbcdn.cn/p/",charset: "utf-8"}]});
 
 KISSY.add('brix/core/mustache', function(S) {
   /*!
