acd4e3c34e0b3d1e9f6328314774072ac8f5d2c7
keyapril <keyapril@gmail.com>
2012-09-12 16:24:09 +0800

    fixed tmpler inDom bug

--- dist/core/tmpler.js
+++ dist/core/tmpler.js
@@ -66,9 +66,16 @@ KISSY.add("brix/core/tmpler", function(S, Mustache, Node,UA) {
          */
         _buildBricks: function(tmpl) {
             var self = this,inDom = false,node,tmplNode;
+            if(typeof tmpl === 'string'){
+                if(tmpl.charAt(0)==='.'||tmpl.charAt(0)==='#'||tmpl==='body'){
+                    node = $(tmpl);
+                }
+            }
+            else{
+                node = tmpl;
+            }
 
-            if(typeof tmpl === 'string'&&( tmpl.charAt(0)==='.'||tmpl.charAt(0)==='#'||tmpl==='body')){
-                node = $(tmpl);
+            if(node){
                 if(node.item(0)[0].tagName.toUpperCase()=='SCRIPT'){
                     //如果是script节点，则直接取html
                     tmpl= node.item(0).html()
@@ -77,10 +84,7 @@ KISSY.add("brix/core/tmpler", function(S, Mustache, Node,UA) {
                     inDom = true;
                 }
             }
-            else{
-                node = tmpl;
-                inDom = true;
-            }
+            
             if (!inDom) {
                 //牛逼的正则啊
                 var reg = /(\{{2,3}\#(.+?)\}{2,3})\s*([\s\S]*)?\s*((\{{2,3})\/\2(\}{2,3}))/g;
@@ -126,7 +130,7 @@ KISSY.add("brix/core/tmpler", function(S, Mustache, Node,UA) {
             } else {
                 tmplNode = node;
             }
-            this.id = _stamp(tmplNode);
+            this.id = _stamp(node);
             var tmplTargetNodes = tmplNode.all('[bx-tmpl-source]');
             tmplTargetNodes.each(function(node) {
                 var selector = node.attr('bx-tmpl-source'),
