802424042ada50df805bf6e7d9c11d143f625285
keyapril <keyapril@gmail.com>
2012-11-26 10:23:37 +0800

    启用全新正则获取子模板

--- dist/1.0/core/tmpler.js
+++ dist/1.0/core/tmpler.js
@@ -4,14 +4,14 @@ KISSY.add("brix/core/tmpler", function(S, Mustache,Node) {
      * 模板解析器，对传入的模板通过钩子进行分析，结合 Mustache 和数据给出 html 片段。
      * @class Brix.Tmpler
      * @param {String}  tmpl    模板字符串
-     * @param {Boolean} isParse 是否需要对模板进行解析
+     * @param {Number} level    对模板进行解析的层级，false表示不解析
      * @requires Brix.Mu
      */
 
-    function Tmpler(tmpl, isParse) {
-        if(tmpl && (isParse !== false)) {
+    function Tmpler(tmpl, level) {
+        if(tmpl && (level !== false)) {
             this.tmpls = [];
-            this._praseTmpl(tmpl);
+            this._praseTmpl(tmpl,level);
         } else {
             this.tmpl = tmpl;
         }
@@ -20,10 +20,11 @@ KISSY.add("brix/core/tmpler", function(S, Mustache,Node) {
     S.extend(Tmpler, Object, {
         /**
          * 解析模板
-         * @param  {String} tmpl 模板字符串
+         * @param  {String} tmpl  模板字符串
+         * @param  {Number} level 对模板进行解析的层级，false表示不解析
          * @private
          */
-        _praseTmpl: function(tmpl) {
+        _praseTmpl: function(tmpl,level) {
             var self = this,
                 inDom = false,
                 node, tmplNode;
@@ -45,28 +46,33 @@ KISSY.add("brix/core/tmpler", function(S, Mustache,Node) {
             }
 
             if(!inDom) {
-                self._buildTmpls(tmpl);
-
+                var r = '<([\\w]+)\\s+[^>]*?bx-tmpl=["\']?([^"\'\\s]+)["\']?\\s+[^>]*?bx-datakey=["\']?([^"\'\\s]+)["\']?[^>]*?>(@brix@)</\\1>';
+                while(level--){
+                    r = r.replace('@brix@','(?:<\\1[^>]*>@brix@</\\1>|[\\s\\S])*?');
+                }
+                r = r.replace('@brix@','(?:[\\s\\S]*?)');
+                self.reg = r;
+                S.log(r);
                 self.tmpl = self._replaceTmpl(tmpl);
+                self._buildTmpls(self.tmpl);
             }
             self.inDom = inDom;
         },
         /**
          * 对节点中的bx-tmpl解析，构建模板和数据配置
-         * @param  {String} tmpl 需要解析的模板
+         * @param  {String} tmpl  需要解析的模板
          * @private
          */
         _buildTmpls: function(tmpl) {
             var self = this;
-            var r = /<!--bx-tmpl="([^"]+?)"\s+bx-datakey="([^"]+?)"-->(\s*([\s\S]*)?\s*)<!--bx-tmpl="\1"-->/g,
-                m;
+            var r = new RegExp(self.reg,"ig");
             while((m = r.exec(tmpl)) !== null) {
                 self.tmpls.push({
-                    name: m[1],
-                    datakey: m[2],
-                    tmpler: new Tmpler(self._replaceTmpl(m[3]), false)
+                    name: m[2],
+                    datakey: m[3],
+                    tmpler: new Tmpler(m[4], false)
                 });
-                self._buildTmpls(m[3]);
+                self._buildTmpls(m[4]);
             }
         },
         /**
