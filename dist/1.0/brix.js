dbee9ed0cf51222ebeadeb87763671159e46ce96
keyapril <keyapril@gmail.com>
2012-12-10 17:59:03 +0800

    format code

--- dist/1.0/brix.js
+++ dist/1.0/brix.js
@@ -1802,44 +1802,44 @@ KISSY.add("brix/core/pagelet", function(S, Chunk) {
     requires: ['./chunk']
 });
 KISSY.add("brix/core/demolet", function(S, Pagelet,IO) {
-        /**
-         * 同步获取默认模板和数据，多在demo页构建中使用
-         * @param  {String} tmpl 模板文件
-         * @param  {Object} data 数据对象
-         * @param  {String} s    分割符号，默认‘@’
-         * @private
-         * @return {Object}      模板和数据的对象{tmpl:tmpl,data:data}
-         */
-        function getTmplData(tmpl,data,s){
-            s = s||'@';
-            data = data || {};
-            var reg = new RegExp('\\{\\{'+s+'(.+)?\\}\\}',"ig");
-            tmpl = tmpl.replace(reg,function($1,$2){
-                S.log($2);
-                var str = '';
-                var p = $2.replace(/\//ig,'_').replace(/\./ig,'_');
-                data[p] = data[p] || {};
-                IO({
-                    url:$2+'template.html',
-                    async:false,
-                    success:function(d , textStatus , xhrObj){
-                        str = '{{#'+p+'}}' + d+'{{/'+p+'}}';
-                    }
-                });
-                IO({
-                    url:$2+'data.json',
-                    async:false,
-                    dataType:'json',
-                    success:function(d , textStatus , xhrObj){
-                        for(var k in d){
-                            data[p][k] = d[k];
-                        }
-                    }
-                });
-                return str;
+    /**
+     * 同步获取默认模板和数据，多在demo页构建中使用
+     * @param  {String} tmpl 模板文件
+     * @param  {Object} data 数据对象
+     * @param  {String} s    分割符号，默认‘@’
+     * @private
+     * @return {Object}      模板和数据的对象{tmpl:tmpl,data:data}
+     */
+    function getTmplData(tmpl,data,s){
+        s = s||'@';
+        data = data || {};
+        var reg = new RegExp('\\{\\{'+s+'(.+)?\\}\\}',"ig");
+        tmpl = tmpl.replace(reg,function($1,$2){
+            S.log($2);
+            var str = '';
+            var p = $2.replace(/\//ig,'_').replace(/\./ig,'_');
+            data[p] = data[p] || {};
+            IO({
+                url:$2+'template.html',
+                async:false,
+                success:function(d , textStatus , xhrObj){
+                    str = '{{#'+p+'}}' + d+'{{/'+p+'}}';
+                }
             });
-            return {tmpl:tmpl,data:data};
-        }
+            IO({
+                url:$2+'data.json',
+                async:false,
+                dataType:'json',
+                success:function(d , textStatus , xhrObj){
+                    for(var k in d){
+                        data[p][k] = d[k];
+                    }
+                }
+            });
+            return str;
+        });
+        return {tmpl:tmpl,data:data};
+    }
 
     /**
      * Brix Demolet 用来构建约定的template.html和data.json的占坑demo页面
