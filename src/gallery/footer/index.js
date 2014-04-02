67cd07370f82605950f5c26a7dd7b22c951f3c49
zuomo <zuomo.xb@taobao.com>
2014-04-02 10:21:09 +0800

    grunt

--- src/gallery/footer/index.js
+++ src/gallery/footer/index.js
@@ -1,23 +1,28 @@
-KISSY.add("brix/gallery/footer/index", function(S, Brick, Ajax) {
+KISSY.add("brix/gallery/footer/index", function(S, Brick, IO) {
     function Footer() {
         Footer.superclass.constructor.apply(this, arguments);
     }
 
+    Footer.ATTRS = {
+        /**
+         * 页脚模式，默认前台模式: simple:后台模式
+         * @cfg {String}
+         */
+        mode: {
+            value: ''
+        }
+    }
     S.extend(Footer, Brick, {
-        render: function () {
-            var el = this.get('el');
-            var url = 'http://www.taobao.com/go/rgn/mm/footer.php';
-
-            new Ajax({
-                url: url,
-                dataType: 'jsonp',
-                data: {
-                    mode: this.userConfig.mode || ''
-                },
-                success: function(res) {
-                  el.html(S.one('<textarea />').html(res).val())
-                }
-            })
+        /**
+         * 重写render方法，jsonp方式获取数据
+         */
+        render: function() {
+            var self = this;
+            IO.jsonp('http://www.taobao.com/go/rgn/mm/footer.php', {
+                mode: self.get('mode')
+            }, function(res) {
+                self.get('el').html(S.one('<textarea />').html(res).val());
+            });
         }
     });
     return Footer;
